f8ff2d8441e74678fb76cbc9af5921a26866b6ca
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-12-21 20:43:28 +0600

    react icon added

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { NavLink,Outlet } from "react-router-dom";
-
+import { IoMdAdd } from "react-icons/io";
 
 const Dashboard = () => {
     return (
@@ -8,7 +8,7 @@ const Dashboard = () => {
                {/* main part  */}
                <div className="w-64 min-h-screen bg-sky-500">
                <ul className="menu p-4 py-auto"> 
-                <li className="font-bold text-xl text-white"><NavLink to="/dashboard/newTask">Add New Task</NavLink></li>
+                <li className="font-bold text-xl text-white"><NavLink to="/dashboard/newTask"><IoMdAdd  className="text-xl font-bold"/>Add New Task</NavLink></li>
                 <li className="font-bold text-xl text-white"><NavLink to="/">Go Home</NavLink></li>
                 
            </ul>
