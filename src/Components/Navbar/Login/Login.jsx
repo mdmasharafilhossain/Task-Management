ec69dbab8c55e344792516fa8b896befb55886b8
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-12-22 11:23:32 +0600

    login page added

--- src/Components/Navbar/Login/Login.jsx
+++ src/Components/Navbar/Login/Login.jsx
@@ -1,9 +1,101 @@
-
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { useContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../AuthProviders/AuthProviders";
+import app from "../../Firebase/Firebase";
+import { Link,  useNavigate } from "react-router-dom";
+const auth = getAuth(app);
 
 const Login = () => {
+    useEffect(()=>{
+        document.title = "TaskHub | Login"
+      },[]);
+      const navigate = useNavigate();
+    const {SignIn,SignInWithGoogle} = useContext(AuthContext)
+    const [errorMessage, setErrorMessage] = useState('');
+    const handleLogin = e =>{
+
+         e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email')
+          const password = form.get('password')
+        SignIn(email,password)
+        .then(result =>{
+          console.log(result.user);
+          Swal.fire({
+              title: 'Log In',
+              text: 'Log In Successfully',
+              icon: 'success',
+              confirmButtonText: 'Ok'
+            })
+            navigate("/dashboard")
+        })
+        .catch(error =>{
+          console.error(error);
+          setErrorMessage(error.message)
+  
+        });
+        
+    }
+
+    const hadleGoogleLogin = e =>{
+        const googleProvider = new GoogleAuthProvider();
+        e.preventDefault();
+        SignInWithGoogle(auth,googleProvider)
+        .then(result=>{
+            console.log(result);
+            navigate("/dashboard");
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+      }
     return (
         <div>
-         <h2>Login</h2>   
+         <div className=" py-60">
+            <div className="mt-[100px] md:mt-10 lg:mt-10 text-center   py-5">
+                <h2 className="text-4xl font-bold text-red-600">Please login</h2>
+
+                <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password"
+                        name="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary bg-red-600 border-none font-bold text-white text-base hover:bg-red-700">Login</button>
+                        
+                       
+                    </div>
+                </form>
+                <p className="font-bold mt-5 text-xl text-red-700">OR</p>
+                <button onClick={hadleGoogleLogin} className="btn btn-primary bg-red-700 border-none mt-10 hover:bg-red-900 text-white">Google Login</button>
+                {
+                    errorMessage && <p className="text-sm font-bold text-red-700">{errorMessage}</p>
+                }
+                <p className="text-xl mt-10">New Here?Please <Link to="/register" className="font-bold text-red-700">Register</Link></p>
+            </div>
+
+
+
+
+
+
+
+
+        </div>   
         </div>
     );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from '../../assets/62ec0ed2413e5264826c4850_TaskHub_Horizontal_Logo.png'
+import { useContext } from "react";
+import { AuthContext } from "../AuthProviders/AuthProviders";
 
 const Navbar = () => {
+    const { user, LogOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        LogOut()
+            .then()
+            .catch()
+    }
 
     const NavLinks = <>
     <li><NavLink
@@ -37,7 +46,7 @@ const Navbar = () => {
     return (
         <div>
             <div className='shadow-lg bg-base-100 fixed top-0 left-0 right-0 z-50'>
-                <div className="navbar bg-base-100 container mx-auto">
+                <div className="navbar bg-base-100 ">
                     <div className="navbar-start">
                         <div className="dropdown">
                             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -47,7 +56,7 @@ const Navbar = () => {
                              {NavLinks}
                             </ul>
                         </div>
-                        <img className='h-10' src={logo} alt="" />
+                        <img className='h-10 lg:ml-10' src={logo} alt="" />
                     </div>
                     <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal  px-1">
@@ -55,23 +64,21 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="navbar-end flex lg:mr-5 lg:space-x-1">
-                        <div>
-                            <button>Sign In</button>
-                        </div>
-                    {/* <div>
+                        
+                    <div>
                             <h2 className="mt-4 text-base font-bold text-orange-600-600">{user?.displayName
 
                             }</h2>
-                        </div> */}
+                        </div> 
                         <div>
-                        {/* <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                     <Link to="/UserProfile"><div className="w-12 rounded-full mt-3">
                                         <img src={user?.photoURL} />
                                     </div></Link>
-                                </label> */}
+                                </label>
                         </div>
                         <div>
-                       {/* {
+                       {
                         user? 
                         <button onClick={handleLogOut} className=" text-red-700 border-4 border-red-600
                         hover:bg-red-100 font-bold text-lg px-4 py-2 rounded-xl">
@@ -79,7 +86,7 @@ const Navbar = () => {
                         : 
                         <Link to="/login"><button className=" text-red-700 border-4 border-red-600
                         hover:bg-red-100 font-bold text-lg px-4 py-2 rounded-xl">Sign In</button></Link>
-                       } */}
+                       }
 
                         </div>
                     </div>
