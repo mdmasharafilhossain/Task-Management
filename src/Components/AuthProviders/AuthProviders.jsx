e91992004f984dad07fc999dc9d17aea18f94b12
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-12-22 11:02:33 +0600

    Auth Providers added

---
+++ src/Components/AuthProviders/AuthProviders.jsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from './../Firebase/Firebase';
+
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProviders = ({children}) => {
+    const googleProvider = new GoogleAuthProvider();
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] =  useState(true);
+    
+    const createUser = (email,password) =>{
+        setLoading(true);
+      return  createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const SignIn = (email,password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const SignInWithGoogle = () =>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider);
+    }
+
+    const LogOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log("user changed",currentUser);
+            setUser(currentUser);
+            
+            setLoading(false);
+        });
+        return () =>{
+            return unSubscribe(true);
+        }
+    },[]);
+
+
+    const AuthInfo ={
+        user,
+        createUser,
+        LogOut,
+        SignIn,
+        loading,
+        SignInWithGoogle,
+    }
+    return (
+        <AuthContext.Provider value={AuthInfo}>
+             {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import {
 } from '@tanstack/react-query'
 import PreviousTask from './Components/Dashboard/PreviouTask/PreviousTask';
 import Login from './Components/Navbar/Login/Login';
+import AuthProviders from './Components/AuthProviders/AuthProviders';
 
 const queryClient = new QueryClient()
 
@@ -64,8 +65,10 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    
+   <AuthProviders>
    <QueryClientProvider client={queryClient}>
    <RouterProvider router={router} />
     </QueryClientProvider>
+   </AuthProviders>
   </React.StrictMode>,
 )
